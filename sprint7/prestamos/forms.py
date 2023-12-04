b8671be4b90ccf0165e50a9845da243292edfd09
Federicoleyria <fedeleyria2016@gmail.com>
2023-12-04 00:54:23 -0300

    form

--- sprint7/prestamos/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-
-class CrearPrestamoForm(forms.ModelForm):
-    class Meta:
-        model = Prestamo
-        fields = ['loan_id','loan_type', 'loan_date', 'loan_total', 'customer_id']
